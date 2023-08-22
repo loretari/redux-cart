af7c7ee462dd7a2b1ef0634e30410d79b371a3ad
loretari <loretari@gmail.com>
2023-08-22 15:08:50 +0300

    init: More Complicated State

--- src/App.js
+++ src/App.js
@@ -10,28 +10,36 @@ import { createStore } from "redux";
 
 // initial store
 const initialStore = {
-    count: 0
+    count: 0,
+    name: "john"
 };
 
 function reducer(state, action) {
     console.log({ state, action })
     if (action.type === "DECREASE") {
-       return { count: state.count - 1 }
+       return { ...state, count: state.count - 1, name: "anna" }
     }
     if (action.type === "INCREASE") {
-        return {count: state.count + 1}
+        return { ...state, count: state.count + 1}
     }
         if (action.type === "RESET") {
-            return {count: 0}
+            return { ...state, count: 0}
         }
+    if (action.type === "CHANGE_NAME") {
+        return { ...state, name: "bobo"}
+    }
     return state;
 }
 
 const store = createStore(reducer, initialStore);
 store.dispatch({type: "DECREASE"})
+store.dispatch({type: "CHANGE_NAME"})
 store.dispatch({type: "RESET"})
 store.dispatch({type: "INCREASE"})
 store.dispatch({type: "INCREASE"})
+store.dispatch({type: "INCREASE"})
+store.dispatch({type: "INCREASE"})
+
 
 console.log(store.getState())
 
