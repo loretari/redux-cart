862970f0996fd5cf5e7edd75a30a3a42cb861d81
loretari <loretari@gmail.com>
2023-08-22 13:36:12 +0300

    init: initial first commit

---
+++ src/cart-items.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        title: "Silver-tone crystal ear cuff",
+        price: 599.99,
+        img:
+            "https://www.net-a-porter.com/variants/images/1647597315260089/in/w920_q60.jpg",
+        amount: 1
+    },
+    {
+        id: 2,
+        title: "GG Marmont gold-tone crystal ring ",
+        price: 499.99,
+        img:
+            "https://www.net-a-porter.com/variants/images/1647597311462891/in/w920_q60.jpg",
+        amount: 1
+    },
+    {
+        id: 3,
+        title: "Puffy Heart gold-plated earrings",
+        price: 699.99,
+        img:
+            "https://www.net-a-porter.com/variants/images/1647597301119120/in/w920_q60.jpg",
+        amount: 1
+    }
+];
