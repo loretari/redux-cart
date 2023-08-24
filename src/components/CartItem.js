5c6b9ae627053a590bfe369ce422a635d512bb93
loretari <loretari@gmail.com>
2023-08-24 09:40:15 +0300

    init: add toggle amount vs increase and decrease

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
-import { REMOVE, INCREASE, DECREASE } from "../actions";
+import { REMOVE, INCREASE, DECREASE, TOGGLE_AMOUNT } from "../actions";
 
 
-const CartItem = ({ img, title, price, amount, remove, increase, decrease  }) => {
+const CartItem = ({
+                      img,
+                      title,
+                      price,
+                      amount,
+                      remove,
+                      increase,
+                      decrease,
+                      toggle
+                  }) => {
     return (
         <div className="cart-item">
             <img src={img} alt={title} />
@@ -20,7 +29,7 @@ const CartItem = ({ img, title, price, amount, remove, increase, decrease  }) =>
                 {/* increase amount */}
                 <button
                     className="amount-btn"
-                    onClick={() => increase()}
+                    onClick={() => toggle("inc")}
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z" />
@@ -35,7 +44,7 @@ const CartItem = ({ img, title, price, amount, remove, increase, decrease  }) =>
                         if (amount === 1) {
                             return remove()
                         } else {
-                          return decrease()
+                          return toggle("dec")
                         }
 
 
@@ -55,9 +64,10 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     const { id, amount } = ownProps;
 
     return {
-        remove: () => dispatch({ type: REMOVE, payload: {id}}),
-        increase: () => dispatch({ type: INCREASE, payload: {id}}),
-        decrease: () => dispatch({ type: DECREASE, payload: {id, amount}})
+        remove: () => dispatch({ type: REMOVE, payload: { id}}),
+        increase: () => dispatch({ type: INCREASE, payload: { id}}),
+        decrease: () => dispatch({ type: DECREASE, payload: { id, amount}}),
+        toggle: (toggle) => dispatch({ type: TOGGLE_AMOUNT, payload: { id, toggle } })
 
 
 
