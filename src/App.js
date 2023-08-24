5a80cc8cfc2d0957b922e2c1987157803ced8098
loretari <loretari@gmail.com>
2023-08-24 10:25:06 +0300

    init: add redux dec tools

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import { Provider } from "react-redux";
 
 
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 function App() {
